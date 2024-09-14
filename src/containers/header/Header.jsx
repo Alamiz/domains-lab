9b1c35fc62ace2c681c33864a215371bbbf6c68b
Alami_ <h.elalami@geoloc-systems.com>
2024-09-14 06:44:23 +0100

    Creating the header componnent

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,13 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className="header">
+      <div className="container">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold text-center text-balance mb-6">Quick and Accurate <span className="text-primary">DNS</span> TXT Record <span className="text-primary">Lookup</span></h1>
+          <p className="max-w-lg text-center leading-10">Upload domains, search DNS TXT records, and download results quickly and accurately.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
