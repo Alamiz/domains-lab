7047c9598a78ce4b6fffed941d7caa2a1b166627
Alami_ <h.elalami@geoloc-systems.com>
2024-09-14 14:08:04 +0100

    How it works section

--- src/components/instructionCard/InstructionCard.jsx
+++ src/components/instructionCard/InstructionCard.jsx
@@ -1,6 +1,10 @@
-const InstructionCard = () => {
+const InstructionCard = ({title, description, icon}) => {
   return (
-    <div>InstructionCard</div>
+    <div className="p-6 rounded bg-primary brightness-110 border-2 border-white/20 text-white">
+      {icon}
+      <p className="text-lg font-bold">{title}</p>
+      <p>{description}</p>
+    </div>
   )
 }
 
--- src/containers/howItWorks/HowItWorks.jsx
+++ src/containers/howItWorks/HowItWorks.jsx
@@ -1,6 +1,20 @@
+import { InstructionCard } from "../../components"
+import { MdOutlineFileUpload } from "react-icons/md";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { MdSearch } from "react-icons/md";
+
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    <section>
+      <div className="container flex flex-col items-center justify-center gap-8 bg-primary rounded p-6">
+        <p className="text-4xl font-bold text-center text-white">How it works</p>
+        <div className="flex flex-col gap-4 md:gap-8 md:grid md:grid-cols-3">
+          <InstructionCard icon={<MdOutlineFileUpload size={40} className="text-primary rounded bg-white p-1.5 mb-3"/>} title="Upload" description="Upload a CSV or text file with your domain list."/>
+          <InstructionCard  icon={<MdSearch size={40} className="text-primary rounded bg-white p-1.5 mb-3"/>} title="Search" description="Enter a keyword to search through the retrieved TXT records."/>
+          <InstructionCard  icon={<MdOutlineFileDownload size={40} className="text-primary rounded bg-white p-1.5 mb-3"/>} title="Download" description="Export a file containing all domains with matching TXT records."/>
+        </div>
+      </div>
+    </section>
   )
 }
 
