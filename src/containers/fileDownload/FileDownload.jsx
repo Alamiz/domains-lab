07017c3e2dd149cd0b94a920ee946cd9bbceb4d4
Alami_ <h.elalami@geoloc-systems.com>
2024-09-14 13:31:58 +0100

    creating the file download section

--- src/containers/fileDownload/FileDownload.jsx
+++ src/containers/fileDownload/FileDownload.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
+import { FaDownload } from "react-icons/fa6";
+
 const FileDownload = () => {
+  const [searchKeyWord, setSearchKeyWord] = useState('')
+
+  const search  = () => {
+    console.log(searchKeyWord)
+    setSearchKeyWord('')
+  }
+
   return (
-    <div>FileDownload</div>
+    <section>
+      <div className="container">
+        {/* Search bar */}
+        <div className="flex gap-4">
+          <input className="border-solid border border-gray-300 rounded px-4 py-2 w-full outline-gray-500" type="text" value={searchKeyWord} onChange={(e) => setSearchKeyWord(e.target.value)}/>
+          <button className="bg-primary rounded px-3 py-3" onClick={search}>
+            <IoSearch color="white" size={20}/>
+          </button>
+        </div>
+
+        {/* Download results */}
+        <div className="mt-8">
+          <p className="text-3xl font-bold">Download results</p>
+          <p className="mt-2">This is the result of your keyword Click to download.</p>
+          <button className="flex bg-primary rounded-lg mt-3 px-3 py-3 text-white items-center justify-center gap-2">Download <FaDownload size={20}/></button>
+        </div>
+      </div>
+    </section>
   )
 }
 
