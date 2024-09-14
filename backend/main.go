ffb652e72bc450ef7a3c3e95b354313e00d81ff9
Alami_ <h.elalami@geoloc-systems.com>
2024-09-14 17:44:17 +0100

    initializing the database

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type DomainRecord struct {
+	Domain     string
+	TxtRecords []string
+}
+
+var collection *mongo.Collection
+
+// Initialize MongoDB
+func initDB() {
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	// Get MongoDB URI from environment
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable.")
+	}
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v\n", err)
+	}
+
+	// Ensure disconnection when done
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v\n", err)
+		}
+	}()
+
+	// Get the collection
+	collection := client.Database("recordlookup").Collection("domains")
+	if collection == nil {
+		log.Fatal("Failed to get collection 'domains'")
+	}
+
+}
+
 func main() {
-	fmt.Println("Hello world")
+	initDB()
 }
