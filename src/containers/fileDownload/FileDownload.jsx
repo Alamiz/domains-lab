32490af8b402424a55ffe61679ba411cc54b0c28
Alami_ <h.elalami@geoloc-systems.com>
2024-09-15 15:54:38 +0100

    Impelmenting the download file

--- src/containers/fileDownload/FileDownload.jsx
+++ src/containers/fileDownload/FileDownload.jsx
@@ -1,39 +1,54 @@
 import { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import { RiLoader3Fill } from "react-icons/ri";
 import { FaDownload } from "react-icons/fa6";
 import { useSearchKeyword } from "../../hooks/useSearchKeyword";
 
 const FileDownload = ({ isFileProcessed }) => {
-  const { filepath, error, searchKeyword } = useSearchKeyword()
+  const { filepath, error, searchKeyword, downloadFile, loading } = useSearchKeyword()
 
   const [keyWord, setKeyWord] = useState('')
+  const [inputError, setInputError] = useState('')
 
   const search  = () => {
+    if (!keyWord) {
+      setInputError('Please enter a keyword')
+      return
+    }
     searchKeyword(keyWord)
     setKeyWord('')
   }
 
   useEffect(() => {
-    console.log(filepath);
-  }, [filepath])
+    if (keyWord) {
+      setInputError('')
+    }
+  }, [keyWord])
 
   return (
     <section>
       <div className="container">
         {/* Search bar */}
-        <div className="flex gap-4">
-          <input className="border-solid border border-gray-300 rounded px-4 py-2 w-full outline-gray-500" disabled={!isFileProcessed} type="text" value={keyWord} onChange={(e) => setKeyWord(e.target.value)}/>
-          <button className="bg-primary rounded px-3 py-3" onClick={search} disabled={!isFileProcessed}>
-            <IoSearch color="white" size={20}/>
-          </button>
+        <div className="flex flex-col">
+          {inputError && <p className="text-sm text-red-500">{inputError}</p>}
+          <div className="flex gap-4 w-full">
+            <input className={`${inputError ? 'border-red-500' : ''} ${!isFileProcessed || loading ? 'bg-gray-200' : ''} border-solid border border-gray-300 rounded px-4 py-2 w-full outline-gray-500`} disabled={!isFileProcessed || loading} type="text" value={keyWord} onChange={(e) => setKeyWord(e.target.value)}/>
+            <button className="bg-primary rounded px-3 py-3" onClick={search} disabled={!isFileProcessed || loading}>
+              {
+                loading ?
+                  <RiLoader3Fill className="animate-spin" color="white" size={24} /> :
+                  <IoSearch color="white" size={24} />
+              }
+            </button>
+          </div>
         </div>
 
         {/* Download results */}
-        <div className="mt-8">
+        {(filepath && !loading) && <div className="mt-8">
           <p className="text-3xl font-bold">Download results</p>
           <p className="mt-2">This is the result of your keyword Click to download.</p>
-          <button className="flex bg-primary rounded-lg mt-3 px-3 py-3 text-white items-center justify-center gap-2">Download <FaDownload size={20}/></button>
-        </div>
+          <button className="flex bg-primary rounded-lg mt-3 px-3 py-3 text-white items-center justify-center gap-2" onClick={() => downloadFile(filepath)}>Download <FaDownload size={20} /></button>
+        </div>}
       </div>
     </section>
   )
--- src/hooks/useSearchKeyword.js
+++ src/hooks/useSearchKeyword.js
@@ -4,9 +4,11 @@ import axios from 'axios';
 export const useSearchKeyword = () => {
     const [filepath, setFilepath] = useState(null);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const searchKeyword = async (keyword) => {
         try {
+            setLoading(true);
             const response = await axios.get(`${import.meta.env.VITE_DOMAINS_LAB_API}/search`, {
                 params: { keyword },
             });
@@ -14,8 +16,27 @@ export const useSearchKeyword = () => {
         } catch (error) {
             setError(error.message);
             console.log(error);
+        } finally {
+            setLoading(false);
         }
     };
 
-    return { filepath, error, searchKeyword };
+    const downloadFile = async (filePath) => {
+        try {
+          const response = await axios.get(`${import.meta.env.VITE_DOMAINS_LAB_API}/download`, {
+            params: { file: filePath },
+            responseType: 'blob',
+          });
+          const blob = new Blob([response.data], { type: 'application/octet-stream' });
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = filePath;
+          a.click();
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+    return { filepath, error, searchKeyword, downloadFile, loading };
 };
