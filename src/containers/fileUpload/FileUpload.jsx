e5692ba4dc9f60b025230db404252fc6f5a07c3b
Alami_ <h.elalami@geoloc-systems.com>
2024-09-16 00:38:48 +0100

    Adding the possibility to upload more than one file

--- src/containers/fileUpload/FileUpload.jsx
+++ src/containers/fileUpload/FileUpload.jsx
@@ -35,6 +35,15 @@ const FileUpload = ({ setIsFileProcessed }) => {
             <FaFileLines size={56} className="text-primary mb-4" />
             <p className="text-xl mb-4">{file.name}</p>
             <ProgressBar progress={progress}/>
+
+            {/* Upload another file */}
+            <div className="flex items-center justify-center gap-4 mt-6">
+              <p className="text-md font-bold">Upload another file ?</p>
+              <button className="flex items-center justify-center gap-2 text-background text-lg bg-primary rounded-full px-4 py-2"
+                onClick={() => fileRef.current.click()} >
+                Click here to upload <FaCloudArrowUp size={24} />
+              </button>
+            </div>
           </div>
         }
         <input className="hidden" ref={fileRef} type="file" accept=".txt" onChange={handleFileChange} />
