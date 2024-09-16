5bf8c3d4b0adee5853d414dd2b0ec63c391f8586
Alami_ <h.elalami@geoloc-systems.com>
2024-09-16 06:49:41 +0100

    Changin the project structure

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -9,14 +9,14 @@ const NavBar = () => {
     <header className="p-6 bg-background/85 backdrop-blur sticky top-0 inset-x-0 z-40 shadow-md">
       <div className="container">
         <nav className="flex items-center justify-between gap-6">
-          <a className="w-52 cursor-pointer" href="#">
+          <a className="w-52 cursor-pointer" href="/">
             <img src="./images/Domain-Lab-Logo.svg" alt="" />
           </a>
           <ul className="flex flex-1 items-center justify-center gap-4 md:flex hidden">
-            <li onClick={() => setCurrentTab('home')}><a href="#" className={currentTab === "home" ? "text-primary underline underline-offset-8" : "text-secondary"}>Home</a></li>
-            <li onClick={() => setCurrentTab('upload')}><a href="#upload" className={currentTab === "upload" ? "text-primary underline underline-offset-8" : "text-secondary"}>Upload File</a></li>
-            <li onClick={() => setCurrentTab('search')}><a href="#search" className={currentTab === "search" ? "text-primary underline underline-offset-8" : "text-secondary"}>Search</a></li>
-            <li onClick={() => setCurrentTab('download')}><a href="#download" className={currentTab === "download" ? "text-primary underline underline-offset-8" : "text-secondary"}>Download</a></li>
+            <li onClick={() => setCurrentTab('home')}><a href="#header" className={currentTab === "home" ? "text-primary underline underline-offset-8" : "text-secondary hover:text-primary"}>Home</a></li>
+            <li onClick={() => setCurrentTab('upload')}><a href="#upload" className={currentTab === "upload" ? "text-primary underline underline-offset-8" : "text-secondary hover:text-primary transition-all"}>Upload File</a></li>
+            <li onClick={() => setCurrentTab('search')}><a href="#search" className={currentTab === "search" ? "text-primary underline underline-offset-8" : "text-secondary  hover:text-primary transition-all"}>Search</a></li>
+            <li onClick={() => setCurrentTab('download')}><a href="#download" className={currentTab === "download" ? "text-primary underline underline-offset-8" : "text-secondary  hover:text-primary transition-all"}>Download</a></li>
           </ul>
           <button className="md:flex hidden bg-primary rounded-md px-3 py-2 text-white">Get started</button>
           <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
@@ -26,7 +26,7 @@ const NavBar = () => {
         </nav>
           {isOpen && (
             <ul className="flex flex-1 flex-col mt-4 items-center justify-center gap-4">
-              <li onClick={() => setIsOpen(false)}><a href="#" className={"text-secondary"}>Home</a></li>
+              <li onClick={() => setIsOpen(false)}><a href="#header" className={"text-secondary"}>Home</a></li>
               <li onClick={() => setIsOpen(false)}><a href="#upload" className="text-secondary">Upload File</a></li>
               <li onClick={() => setIsOpen(false)}><a href="#search" className="text-secondary">Search</a></li>
               <li onClick={() => setIsOpen(false)}><a href="#download" className="text-secondary">Download</a></li>
